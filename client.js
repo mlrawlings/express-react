e9b1547db287f48d8194932106cbebb98fba8670
Michael Rawlings <mlrawlings@embarkweb.com>
2015-08-27 23:18:45 -0400

    first commit

---
+++ client.js
@@ -0,0 +1,28 @@
+var React = require('react')
+  , assign = require('object-assign')
+
+var DEFAULT_OPTIONS = {
+	mountNodeId: 'app',
+	moduleRoot: null
+}
+
+module.exports = function(options){
+	options = assign({}, DEFAULT_OPTIONS, options || {})
+
+	return function expressReact(req, res, next) {
+		res.document = { doctype:'html' }
+		res.render = createClientRenderer(req, res, next)
+		next()
+	}
+
+	function createClientRenderer(req, res, next) {
+		return function render(Component, props) {
+			var element = React.createElement(Component, props)
+			  , mountNode = document.getElementById(options.mountNodeId)
+
+			document.title = res.document.title
+
+			React.render(element, mountNode)
+		}
+	}
+}
